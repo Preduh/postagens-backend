55cd0ad24f1ca572efae6308deb79f534eb906c8
Preduh <pedro.lucas5860@gmail.com>
2021-01-31 22:19:44 -0300

    start node

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/postagem', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://deploy:147258369.@cluster0.hksoi.mongodb.net/posts?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
 module.exports = mongoose
